e8dccdc03fd1e286838a7dcddef3473a90c5d12e
4mmZ <andresmaxi111014@gmail.com>
2023-02-24 20:25:19 -0800

    ading tests cases

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,9 +7,17 @@ from models.base_model import BaseModel
 from datetime import datetime
 
 class Test_BaseModel(unittest.TestCase):
-
     def test_actual_time(self):
         my_model = BaseModel()
         my_model.save()
         time = datetime.now()
         self.assertFalse(my_model.updated_at == time)
+
+    def test_to_dict(self):
+        diccionary = BaseModel()
+        dict = diccionary.to_dict()
+        self.assertEqual(dict['id'], diccionary.id)
+        self.assertEqual(dict['created_at'], diccionary.created_at.isoformat())
+        self.assertEqual(dict['updated_at'], diccionary.updated_at.isoformat())
+        self.assertEqual(dict['__class__'], 'BaseModel')
+
