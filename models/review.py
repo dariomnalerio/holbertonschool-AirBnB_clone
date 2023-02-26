d25b51709d86073fe330d06a8686173de3d25917
dariomnalerio <dariomnalerio@gmail.com>
2023-02-26 02:19:16 -0300

    Add task 9

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Module containing class User """
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """
+    Class Review
+    """
+palce_id = ""
+user_id = ""
+text = ""
