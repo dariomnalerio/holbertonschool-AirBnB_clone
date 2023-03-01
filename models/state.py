641bb9854b8bbbebec75918d4a60bb4eb6ad1827
dariomnalerio <dariomnalerio@gmail.com>
2023-02-28 21:36:36 -0300

    Update documentation and remove unnecesary files

--- models/state.py
+++ models/state.py
@@ -5,6 +5,6 @@ from models.base_model import BaseModel
 
 class State(BaseModel):
     """
-    Class State
+    Representation of a sate
     """
-    name = "" # Name of the state
+    name = ""  # Name of the state
